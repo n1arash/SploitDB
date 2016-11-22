555b276da77315fafcc4c7c95817eda24c5bc885
Arash Khazaei <0xclay@gmail.com>
2016-11-22 17:16:26 +0330

    added exploit to admin interface and fixed site index

--- SploitDB/urls.py
+++ SploitDB/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from exploit import views
+
 
 urlpatterns = [
+    url(r'$^', views.index,name="index"),
     url(r'^exploit/', include('exploit.urls')),
     url(r'^admin/', admin.site.urls),
 ]
